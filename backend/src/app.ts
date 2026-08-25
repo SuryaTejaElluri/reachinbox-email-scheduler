7fb34ea166b0a74a9cdc5233d0c7984f8aacdfd3
Surya <surya09e@gmail.com>
2026-08-25 20:37:06 +0530

    code fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,12 @@ const app = express();
 /*
  * Global middleware
  */
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 /*
